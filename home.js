41a265f7757a368c6295a555a224a3e3f4601af6
Najah Sadmim <136795896+najahsadmim@users.noreply.github.com>
2026-07-13 15:50:02 +0600

    Update

--- home.js
+++ home.js
@@ -296,3 +296,57 @@ if (homeProfileButton && homeProfileMenu) {
         }
     );
 }
+
+const homeProfileButton = document.getElementById(
+    "profileButton"
+);
+
+const homeProfileMenu = document.getElementById(
+    "profileMenu"
+);
+
+const homeLogoutButton = document.getElementById(
+    "logoutButton"
+);
+
+if (homeProfileButton && homeProfileMenu) {
+    homeProfileButton.addEventListener(
+        "click",
+        event => {
+            event.stopPropagation();
+
+            homeProfileMenu.classList.toggle(
+                "show"
+            );
+        }
+    );
+
+    homeProfileMenu.addEventListener(
+        "click",
+        event => {
+            event.stopPropagation();
+        }
+    );
+
+    document.addEventListener(
+        "click",
+        () => {
+            homeProfileMenu.classList.remove(
+                "show"
+            );
+        }
+    );
+}
+
+if (homeLogoutButton) {
+    homeLogoutButton.addEventListener(
+        "click",
+        () => {
+            localStorage.removeItem(
+                "busLagbeCurrentUser"
+            );
+
+            window.location.href = "index.html";
+        }
+    );
+}
