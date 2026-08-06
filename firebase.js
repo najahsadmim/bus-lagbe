c3157d5615bba96e1fb5b212040a0280edf6e7b5
Najah Sadmim <136795896+najahsadmim@users.noreply.github.com>
2026-08-06 20:17:56 +0600

    update firebase

--- firebase.js
+++ firebase.js
@@ -1,23 +1,46 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
 
 import {
     getAuth,
     GoogleAuthProvider,
-    signInWithPopup
-} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+    browserLocalPersistence,
+    setPersistence
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
+
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+
+import {
+    getStorage
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-storage.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB6cq46hL9Qj_UUo1mkd5Mhv4hMm-UyaL4",
-    authDomain: "bus-lagbe-5a203.firebaseapp.com",
-    projectId: "bus-lagbe-5a203",
-    storageBucket: "bus-lagbe-5a203.firebasestorage.app",
-    messagingSenderId: "669023691866",
-    appId: "1:669023691866:web:4a6230815721cce0c5a463"
+
+    apiKey: "AIzaSyCNT1wlnX35rWndmdFQcSc-8gVtwxZKiVk",
+
+    authDomain: "herwill-sprout.firebaseapp.com",
+
+    projectId: "herwill-sprout",
+
+    storageBucket: "herwill-sprout.firebasestorage.app",
+
+    messagingSenderId: "799323507693",
+
+    appId: "1:799323507693:web:7d4fccfde70ae3007875e4"
+
 };
 
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
+
+setPersistence(auth, browserLocalPersistence)
+    .catch(error => console.error(error));
+
+const db = getFirestore(app);
+
+const storage = getStorage(app);
 
 const googleProvider = new GoogleAuthProvider();
 
@@ -26,7 +49,13 @@ googleProvider.setCustomParameters({
 });
 
 export {
+
     auth,
-    googleProvider,
-    signInWithPopup
+
+    db,
+
+    storage,
+
+    googleProvider
+
 };
